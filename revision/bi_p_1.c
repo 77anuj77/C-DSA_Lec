2d00760331a8386ea01b9c1e37c7337124b4e4bd
77anuj77 <anujparoha07@gmail.com>
2026-07-30 10:49:37 +0530

    updated the code

--- revision/bi_p_1.c
+++ revision/bi_p_1.c
@@ -1,34 +1,57 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int biwordsearch(char words[], char target[], int size){
-    int low =0, high=size-1, mid;
-    int found=0;
-    int index;
-    while (low<=high){
-        mid=(low+high)/2;
+#include <stdio.h>
+#include <string.h>
+
+int biwordsearch(char *words[], char *target[], int size)
+{
+    int low = 0, high = size - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
         int cmp = strcmp(words[mid], target);
-        if (cmp){
-            found =1;
-            index= mid;
-            break;
+
+        if (cmp == 0)
+        {
+            return mid;
         }
-        else if (cmp>0){
-            high=mid-1;
+        else if (cmp > 0)
+        {
+            high = mid - 1;
         }
-        else{
-            low =mid+1;
+        else
+        {
+            low = mid + 1;
         }
     }
-    printf("The target word is at index %d", index);
+
+    return -1;
 }
-int main(){
-    char target[20];
+
+int main()
+{
+    char *target[20];
+
     char *words[] = {
-        "apple", "banana", "cherry", "grape", "orange", "peach"
+        "apple",
+        "banana",
+        "cherry",
+        "grape",
+        "orange",
+        "peach"
     };
-    int size= (sizeof(words)/sizeof(words[0]));
-    printf("Enter the Target: ");
-    scanf("%s" ,&target );
-    biwordsearch(words, target, size);
+
+    int size = sizeof(words) / sizeof(words[0]);
+
+    printf("Enter target word: ");
+    scanf("%s", target);
+
+    int index = biwordsearch(words, target, size);
+
+    if (index != -1)
+        printf("The target word is at index %d\n", index);
+    else
+        printf("Word not found\n");
+
+    return 0;
 }
